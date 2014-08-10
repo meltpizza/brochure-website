03597e2755748713f6e99240d2c2306872b60964
Wee Keat Chin <weekeat@visualdensity.com>
2014-08-11 08:23:15 +1000

    Flesh out partner code

---
+++ src/Melt/WebsiteBundle/Controller/PartnerController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Melt\WebsiteBundle\Controller;
+
+use Melt\WebsiteBundle\Entity\Media;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+/**
+ * @Route("/partner")
+ */
+class PartnerController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Cache(expires="+2 days", public="true")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return $this->redirect($this->generateUrl('page_home'));
+    }
+
+    /**
+     * @Route("/{partner_code}")
+     * @Template()
+     */
+    public function parnerAction()
+    {
+        return $this->redirect($this->generateUrl('page_home'));
+    }
+}//PartnerController
