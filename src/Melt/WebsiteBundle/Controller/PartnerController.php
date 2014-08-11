b64ab480c1fe365a504846858200c9e3d33db93d
Wee Keat Chin <weekeat@visualdensity.com>
2014-08-11 22:37:32 +1000

    Fleshing out client form

--- src/Melt/WebsiteBundle/Controller/PartnerController.php
+++ src/Melt/WebsiteBundle/Controller/PartnerController.php
@@ -2,7 +2,9 @@
 
 namespace Melt\WebsiteBundle\Controller;
 
-use Melt\WebsiteBundle\Entity\Media;
+use Melt\WebsiteBundle\Entity\Partner;
+use Melt\WebsiteBundle\Entity\PartnerEntry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
@@ -15,7 +17,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class PartnerController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="partner_index")
      * @Cache(expires="+2 days", public="true")
      * @Template()
      */
@@ -25,11 +27,28 @@ class PartnerController extends Controller
     }
 
     /**
-     * @Route("/{partner_code}")
+     * @Route("/{partner_code}", name="partner_page")
      * @Template()
      */
-    public function partnerAction()
+    public function partnerAction($partner_code)
     {
-        return array();
+        $partner = $this->getDoctrine()->getRepository('WebsiteBundle:Partner')->findOneBy(array('code'=>$partner_code));
+        $entry   = new PartnerEntry();
+
+        if(!$partner || !$partner->getActive() ) {
+            return $this->redirect($this->generateUrl('page_home'));
+        }
+
+        $form = $this->createFormBuilder($entry)
+                     ->setAction($this->generateUrl('partner_page', array('partner_code'=>$partner_code)))
+                     ->add('name'  , 'text')
+                     ->add('email' , 'text')
+                     ->add('save'  , 'submit')
+                     ->getForm();
+
+        return array(
+            'form'    => $form->createView(),
+            'partner' => $partner
+        );
     }
 }//PartnerController
