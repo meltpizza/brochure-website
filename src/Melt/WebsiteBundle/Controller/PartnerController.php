d8737f49595e11e9309697cdb8c01381675b2e19
Wee Keat Chin <weekeat@visualdensity.com>
2014-08-17 16:24:08 +1000

    Save entries

--- src/Melt/WebsiteBundle/Controller/PartnerController.php
+++ src/Melt/WebsiteBundle/Controller/PartnerController.php
@@ -5,9 +5,11 @@ namespace Melt\WebsiteBundle\Controller;
 use Melt\WebsiteBundle\Entity\Partner;
 use Melt\WebsiteBundle\Entity\PartnerEntry;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 
@@ -17,14 +19,49 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class PartnerController extends Controller
 {
     /**
-     * @Route("/", name="partner_index")
-     * @Cache(expires="+2 days", public="true")
+     * @Route("/submit", name="partner_submit")
      * @Template()
      */
-    public function indexAction()
+    public function partnerSubmitAction(Request $request)
     {
-        return $this->redirect($this->generateUrl('page_home'));
+        $session = $this->get('session');
+        $code    = $request->get('code');
+
+        $partner = $this->getDoctrine()
+                        ->getRepository('WebsiteBundle:Partner')
+                        ->findOneBy(array('code'=>$code));
+
+        if(!$partner || !$partner->getActive() ) {
+            $session->getFlashBag()->add('notice', "Sorry but partner can't be found!");
+            return $this->redirect($this->generateUrl('page_home'));
+        }
+
+        $entry = new PartnerEntry();
+        $form  = $this->createFormBuilder($entry)
+                      ->setAction($this->generateUrl('partner_submit', array('code' => $code)))
+                      ->add('name'  , 'text')
+                      ->add('email' , 'text')
+                      ->add('submit', 'submit')
+                      ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $entry->setCreated(new \DateTime());
+            $entry->setPartner($partner);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entry);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add('success', 'Your entry have been saved');
+            return $this->redirect($this->generateUrl('partner_page', array('partner_code'=>$code)));
+
+        } else {
+            $session->getFlashBag()->add('notice', "Sorry but your entry cannot be saved.");
+            return $this->redirect($this->generateUrl('page_home'));
+        }
     }
+
 
     /**
      * @Route("/{partner_code}", name="partner_page")
@@ -32,7 +69,11 @@ class PartnerController extends Controller
      */
     public function partnerAction($partner_code)
     {
-        $partner = $this->getDoctrine()->getRepository('WebsiteBundle:Partner')->findOneBy(array('code'=>$partner_code));
+        $session = $this->get('session');
+        $partner = $this->getDoctrine()
+                        ->getRepository('WebsiteBundle:Partner')
+                        ->findOneBy(array('code'=>$partner_code));
+
         $entry   = new PartnerEntry();
 
         if(!$partner || !$partner->getActive() ) {
@@ -40,10 +81,10 @@ class PartnerController extends Controller
         }
 
         $form = $this->createFormBuilder($entry)
-                     ->setAction($this->generateUrl('partner_page', array('partner_code'=>$partner_code)))
+                     ->setAction($this->generateUrl('partner_submit', array('code' => $partner_code)))
                      ->add('name'  , 'text')
                      ->add('email' , 'text')
-                     ->add('save'  , 'submit')
+                     ->add('submit', 'submit')
                      ->getForm();
 
         return array(
@@ -51,4 +92,15 @@ class PartnerController extends Controller
             'partner' => $partner
         );
     }
+
+    /**
+     * @Route("/", name="partner_index")
+     * @Cache(expires="+2 days", public="true")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        //return $this->redirect($this->generateUrl('page_home'));
+    }
+
 }//PartnerController
